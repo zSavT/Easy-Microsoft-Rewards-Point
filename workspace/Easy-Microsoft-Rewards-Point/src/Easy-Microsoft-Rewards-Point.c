4bd8cf5607139a93a55b83bdfdbc95629fc7d3cc
zSavT <francesco.saverio.cassano@gmail.com>
2023-11-24 14:28:06 +0100

    refactor code

--- workspace/Easy-Microsoft-Rewards-Point/src/Easy-Microsoft-Rewards-Point.c
+++ workspace/Easy-Microsoft-Rewards-Point/src/Easy-Microsoft-Rewards-Point.c
@@ -10,91 +10,42 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
-#include <time.h>
-#include <stdlib.h>
 #include <string.h>
 #include <Windows.h>
 #include "randomString.h"
 
 #define NUMBER_SEARCH 36
+#define FILE_PATH "C:/EasyMicrosoftPoint/time.txt"
 
 void punti();
-bool isEmpty(int i);
+bool isEmpty();
 char* concatenateStrings(const char* string1, const char* string2, const char* string3);
+void writeDateToFile(const char* filePath);
+void readDateFromFile(const char* filePath, int temp[]);
 
 int main(void) {
 
-	//READ VAL ON FILE TIME.TXT
 	time_t t = time(NULL);
-	struct tm tm = *localtime(&t);
-	int temp[3];					//file time
-	int i = 0;
-	FILE *fp;
-	fp = fopen("C:/EasyMicrosoftPoint/time.txt", "r"); // read mode
-	if (fp == NULL) {
-		perror("Error while opening the file.\n");
-		exit(-1);
-	}
-	printf("Day, month and year saved on file.\n");
-	do {
-		fscanf(fp, "%d", &temp[i]);			//read val from file
-		printf("%d\n", temp[i]);
-		i++;
-	} while (!feof(fp));
-	fclose(fp);
+    struct tm tm = *localtime(&t);
+    int temp[3];  // file time
 
-	if (isEmpty(i) == TRUE)	// if file is empty, write today value and run points script
-	{
-		fp = fopen("C:/EasyMicrosoftPoint/time.txt", "w"); // write mode
-		if (fp == NULL) {
-			perror("Error while opening the file.\n");
-			system("PAUSE");
-			exit(-2);
-		} else {
-			tm = *localtime(&t);						//reset val
-			fprintf(fp, "%d\n", tm.tm_mday);			//write day on file
-			fprintf(fp, "%d\n", tm.tm_mon + 1);			//write mouth on file
-			fprintf(fp, "%d", tm.tm_year + 1900);		//write year on file
-		}
-		fclose(fp);
-		punti();
-	} else {
-		//CHECK ACTUAL DAY MONTH YEAR WITH DAY MONTH YEAR ON FILE
-		i = 0;
-		if (temp[2] == tm.tm_year + 1900 && temp[0] == tm.tm_mday
-				&& temp[1] == tm.tm_mon + 1) {
-			return EXIT_SUCCESS;
-		} else if (temp[0] == tm.tm_mday && temp[1] == tm.tm_mon + 1
-				&& temp[2] < tm.tm_year + 1900) {
-			punti();
-		} else if (temp[0] == tm.tm_mday && temp[1] < tm.tm_mon + 1
-				&& temp[2] <= tm.tm_year + 1900) {
-			punti();
-		} else if (temp[0] > tm.tm_mday && temp[1] < tm.tm_mon + 1
-				&& temp[2] <= tm.tm_year + 1900) {
-			punti();
-		}
+    readDateFromFile(FILE_PATH, temp);
 
-		fp = fopen("C:/EasyMicrosoftPoint/time.txt", "w"); // write mode
-		if (fp == NULL) {
-			perror("Error while opening the file.\n");
-			system("PAUSE");
-			exit(-2);
-		} else {
-			tm = *localtime(&t);						//reset val
-			fprintf(fp, "%d\n", tm.tm_mday);			//write day on file
-			fprintf(fp, "%d\n", tm.tm_mon + 1);			//write mouth on file
-			fprintf(fp, "%d", tm.tm_year + 1900);		//write year on file
-		}
-		fclose(fp);
-	}
+    if (isEmpty()) {
+        writeDateToFile(FILE_PATH);
+        punti();
+    } else {
+        // CHECK ACTUAL DAY MONTH YEAR WITH DAY MONTH YEAR ON FILE
+        if (temp[2] < tm.tm_year + 1900 || (temp[2] == tm.tm_year + 1900 && (temp[1] < tm.tm_mon + 1 ||
+                                      (temp[1] == tm.tm_mon + 1 && temp[0] < tm.tm_mday)))) {
+            punti();
+        }
 
+        writeDateToFile(FILE_PATH);
+    }
 
-	return EXIT_SUCCESS;
-
-
+    return EXIT_SUCCESS;
 }
-
 /*
  * starting from the generated string, the method opens an edge page with the search of the generated string
  */
@@ -113,21 +64,27 @@ void punti() {
 	system("taskkill /F /IM msedge.exe");
 }
 
-bool isEmpty(int i) {
-	if (i > 1)		// if file is empty, do while loop make just one iterations
-		return FALSE;
-	else
-		return TRUE;
+/*
+ * Returns true if the file is empty, false otherwise
+ */
+bool isEmpty() {
+    FILE* file = fopen(FILE_PATH, "r");
+    if (file == NULL) {
+        perror("Error opening the file.\n");
+        return false;
+    }
+    int character = fgetc(file);
+    fclose(file);
+    return (character == EOF);
 }
 
+/*
+ * Take string1, string2, string3 and return string1+string2+string3
+ */
 char* concatenateStrings(const char* string1, const char* string2, const char* string3) {
 
     size_t totalLength = strlen(string1) + strlen(string2) + strlen(string3) + 1; // Calculate the total length of the strings
-
-
     char* result = (char*)malloc(totalLength); // Allocate memory for the result string
-
-
     if (result == NULL) {	// Check if memory allocation was successful
         perror("Error during memory allocation");
         exit(EXIT_FAILURE);
@@ -139,5 +96,43 @@ char* concatenateStrings(const char* string1, const char* string2, const char* s
     strcat(result, string3);
 
     return result;
+}
+
+/*
+ * Write on file date val
+ */
+void writeDateToFile(const char* filePath) {
+    FILE* fp = fopen(filePath, "w");  // write mode
+    if (fp == NULL) {
+        perror("Error while opening the file.\n");
+        system("PAUSE");
+        exit(EXIT_FAILURE);
+    } else {
+        time_t t = time(NULL);
+        struct tm tm = *localtime(&t);
+        fprintf(fp, "%d\n", tm.tm_mday);        // write day on file
+        fprintf(fp, "%d\n", tm.tm_mon + 1);     // write mouth on file
+        fprintf(fp, "%d", tm.tm_year + 1900);   // write year on file
+    }
+    fclose(fp);
+}
+
+/*
+ * Read from file date val
+ */
+void readDateFromFile(const char* filePath, int temp[]) {
+    FILE* fp = fopen(filePath, "r");  // read mode
+    if (fp == NULL) {
+        perror("Error while opening the file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Day, month, and year saved on file.\n");
+    for (int i = 0; i < 3; i++) {
+        fscanf(fp, "%d", &temp[i]);  // read val from file
+        printf("%d\n", temp[i]);
+    }
+
+    fclose(fp);
 }
 
